68945f9abdff716b0d0052268cf338007192b9b9
sthirocean <98005634+sthirocean@users.noreply.github.com>
2023-11-09 15:38:14 +0530

    Initial Commit for file: data.js

---
+++ data.js
@@ -0,0 +1,134 @@
+export const data = {
+    "394d2bd1-9aba-435e-92e5-e2136d07339f": {
+        "id": "394d2bd1-9aba-435e-92e5-e2136d07339f",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": ""
+    },
+    "d94f550d-2981-4918-8aae-bd4c6d6cd5f4": {
+        "id": "d94f550d-2981-4918-8aae-bd4c6d6cd5f4",
+        "parent_id": "394d2bd1-9aba-435e-92e5-e2136d07339f",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "new_chat": true,
+        "search": false
+    },
+    "3eeae1e7-25d8-4c16-b1e6-9269e2e96fff": {
+        "id": "3eeae1e7-25d8-4c16-b1e6-9269e2e96fff",
+        "parent_id": "d94f550d-2981-4918-8aae-bd4c6d6cd5f4",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/547.png",
+        "contact_name": "tax",
+        "last_message": "will",
+        "timestamp": "attorney"
+    },
+    "52bebd0a-5e90-41e7-a92f-cdf279983046": {
+        "id": "52bebd0a-5e90-41e7-a92f-cdf279983046",
+        "parent_id": "d94f550d-2981-4918-8aae-bd4c6d6cd5f4",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/701.png",
+        "contact_name": "attorney",
+        "last_message": "tax",
+        "timestamp": "suffer"
+    },
+    "e8e49484-a2c0-4ead-b2da-e5ae92d11e98": {
+        "id": "e8e49484-a2c0-4ead-b2da-e5ae92d11e98",
+        "parent_id": "d94f550d-2981-4918-8aae-bd4c6d6cd5f4",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/587.png",
+        "contact_name": "throw",
+        "last_message": "clear",
+        "timestamp": "throw"
+    },
+    "673a8bca-6c19-4c3c-82c2-e72c44256597": {
+        "id": "673a8bca-6c19-4c3c-82c2-e72c44256597",
+        "parent_id": "394d2bd1-9aba-435e-92e5-e2136d07339f",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "new_chat": true,
+        "search": false
+    },
+    "e993c294-9037-4440-b646-398bc01e2681": {
+        "id": "e993c294-9037-4440-b646-398bc01e2681",
+        "parent_id": "673a8bca-6c19-4c3c-82c2-e72c44256597",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png",
+        "contact_name": "stage",
+        "last_message": "throw",
+        "timestamp": "clear"
+    },
+    "11d55131-35b5-45b3-8356-39dc26198672": {
+        "id": "11d55131-35b5-45b3-8356-39dc26198672",
+        "parent_id": "673a8bca-6c19-4c3c-82c2-e72c44256597",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/704.png",
+        "contact_name": "oil",
+        "last_message": "stage",
+        "timestamp": "suffer"
+    },
+    "ccf10be0-ac95-4361-aa0b-74aa09104bf5": {
+        "id": "ccf10be0-ac95-4361-aa0b-74aa09104bf5",
+        "parent_id": "673a8bca-6c19-4c3c-82c2-e72c44256597",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/27.png",
+        "contact_name": "clear",
+        "last_message": "tax",
+        "timestamp": "good"
+    },
+    "7ee8bc4b-4b10-4b17-8891-26e81e4fff49": {
+        "id": "7ee8bc4b-4b10-4b17-8891-26e81e4fff49",
+        "parent_id": "394d2bd1-9aba-435e-92e5-e2136d07339f",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "new_chat": true,
+        "search": false
+    },
+    "3f41f61c-8ca8-4d79-a2f1-db99b3abf520": {
+        "id": "3f41f61c-8ca8-4d79-a2f1-db99b3abf520",
+        "parent_id": "7ee8bc4b-4b10-4b17-8891-26e81e4fff49",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/303.png",
+        "contact_name": "person",
+        "last_message": "clear",
+        "timestamp": "clear"
+    },
+    "320ad7e9-1f95-4dfd-83a7-c4ab27de2874": {
+        "id": "320ad7e9-1f95-4dfd-83a7-c4ab27de2874",
+        "parent_id": "7ee8bc4b-4b10-4b17-8891-26e81e4fff49",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/395.png",
+        "contact_name": "throw",
+        "last_message": "attorney",
+        "timestamp": "will"
+    },
+    "ceea091e-97c9-4257-8309-363d99f22741": {
+        "id": "ceea091e-97c9-4257-8309-363d99f22741",
+        "parent_id": "7ee8bc4b-4b10-4b17-8891-26e81e4fff49",
+        "index": 0,
+        "created_at": "09/27/2023 05:50:40",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/545.png",
+        "contact_name": "clear",
+        "last_message": "person",
+        "timestamp": "stage"
+    }
+}
